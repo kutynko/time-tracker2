2f302e4aa097a314ebf74ed98e898123dbacbd81
Ruslan Kutynko <kutynko@hotmail.com>
2022-10-29 23:32:53 +0300

    soft delete для проектов

--- server/services/projects.service.ts
+++ server/services/projects.service.ts
@@ -2,14 +2,18 @@ import { db } from "../database";
 
 export function getUserProjects(userId: number) {
   return db.project.findMany({
-    where: { assignments: { every: { userId } } },
+    where: { assignments: { every: { userId } }, deleted: false },
     include: { assignments: true },
   });
 }
 
 export function getProjectById(projectId: number, userId: number) {
   return db.project.findFirst({
-    where: { id: projectId, assignments: { every: { userId } } },
+    where: {
+      id: projectId,
+      assignments: { every: { userId } },
+      deleted: false,
+    },
     include: { assignments: true },
   });
 }
@@ -23,7 +27,7 @@ export function createNewProject(title: string, userId: number) {
 
 export async function updateProject(id: number, title: string, userId: number) {
   const project = await db.project.findFirst({
-    where: { id, assignments: { every: { userId } } },
+    where: { id, assignments: { every: { userId } }, deleted: false },
   });
   if (project) {
     return db.project.update({ data: { title }, where: { id } });
@@ -32,9 +36,9 @@ export async function updateProject(id: number, title: string, userId: number) {
 
 export async function removeProject(id: number, userId: number) {
   const project = await db.project.findFirst({
-    where: { id, assignments: { every: { userId } } },
+    where: { id, assignments: { every: { userId } }, deleted: false },
   });
   if (project) {
-    return db.project.delete({ where: { id } });
+    return db.project.update({ data: { deleted: true }, where: { id } });
   }
 }
